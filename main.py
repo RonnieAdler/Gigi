aba4b0dda22bdb8133e974bc71d5a1205de20ef1
furry-waffle <67923889+RonnieAdler@users.noreply.github.com>
2023-04-04 01:57:43 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,69 @@
+import datetime
+
+import speech_recognition as sr
+import pyttsx3
+import pywhatkit
+import wikipedia
+import pyjokes
+
+listener = sr.Recognizer()
+engine = pyttsx3.init()
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[1].id)
+
+engine.say("Hi, My name is Gigi. What is your name")
+
+engine.runAndWait()
+
+
+def talk(text):
+    engine.say(text)
+    print(text)
+    engine.runAndWait()
+
+
+def take_command():
+    try:
+        with sr.Microphone() as source:
+            print('listeni....')
+            print('ki')
+            voice = listener.listen(source)
+            command = listener.recognize_google(voice)
+            command1 = command.lower()
+            if 'gigi' in command1:
+                command1 = command1.replace('gigi', '')
+                talk(command1)
+                return command1
+            else:
+                return 0
+
+    except():
+      pass
+
+
+def run_alexa():
+    command = take_command()
+    print(command)
+    if 'play' in command:
+        song = command.replace('play', '')
+        talk('playing'+song)
+        pywhatkit.playonyt(song)
+    elif 'time' in command:
+        time = datetime.datetime.now().strftime('%H:%M %p')
+        print(time)
+        talk('Current time is' + time)
+    elif 'who is' or 'what is' or 'search' or 'search about' in command:
+        person = command.replace('who is' or 'what is' or 'search about', '')
+        info = wikipedia.summary(person, 1)
+        print(info)
+        talk(info)
+    elif 'date' or 'love' in command:
+        talk('I am in relationship with Jarvis')
+    elif 'joke' in command:
+        talk(pyjokes.get_joke())
+    else:
+        talk('Please say the command again.')
+
+
+while True:
+    run_alexa()
